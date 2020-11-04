c8cfa9a079c3b6aa7a368c10ba8245babedf7fd6
Kadet <techkadet@gmail.com>
2020-11-04 19:02:05 +0100

    added proucts page

--- src/pages/productsServices/ProductsServices.jsx
+++ src/pages/productsServices/ProductsServices.jsx
@@ -7,6 +7,42 @@ function ProductsServices() {
       <div className='page-title'>
         <h1>Products & Services</h1>
       </div>
+
+      <div className="section-one">
+        <h2>Our Services</h2>
+
+        <ul>
+          {
+            [
+              "Elevator sales",
+              "General Repairs",
+              "Servicing and maintenance",
+              'Overhauling'
+            ].map(item => (
+            <li>{item}</li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <div className="section-three">
+        <h2>Our Products</h2>
+
+        <ul>
+          {
+            [
+              "Elevators",
+              "Escalators",
+              "Supply gears",
+              'Bearings'
+            ].map(item => (
+            <li>{item}</li>
+            ))
+          }
+        </ul>
+      </div>
+
+
     </div>
   )
 }
