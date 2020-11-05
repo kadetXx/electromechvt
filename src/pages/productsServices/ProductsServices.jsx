46218ebc5ae580e3722e5c10afa974d545462143
Kadet <techkadet@gmail.com>
2020-11-05 09:08:16 +0100

    added products and services page

--- src/pages/productsServices/ProductsServices.jsx
+++ src/pages/productsServices/ProductsServices.jsx
@@ -1,50 +1,96 @@
-import React from 'react'
-import './ProductsServices.scss'
+import React, {useState} from "react";
+import "./ProductsServices.scss";
 
 function ProductsServices() {
+
+  const [services, toggleServices] = useState(0)
+  const [products, toggleProducts] = useState(0)
+
   return (
     <div id='ProductsServices'>
       <div className='page-title'>
         <h1>Products & Services</h1>
       </div>
 
-      <div className="section-one">
+      <div className='section-one'>
         <h2>Our Services</h2>
 
         <ul>
-          {
-            [
-              "Elevator sales",
-              "General Repairs",
-              "Servicing and maintenance",
-              'Overhauling'
-            ].map(item => (
-            <li>{item}</li>
-            ))
-          }
+          {[
+            {
+              title: "Elevator sales",
+              desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+            },
+            {
+              title: "General Repairs",
+              desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+            },
+            {
+              title: "Servicing and maintenance",
+              desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+            },
+            {
+              title: "Overhauling",
+              desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+            },
+          ].map((item, index) => (
+            <li>
+              <span onClick={() => services === index ? toggleServices(null) : toggleServices(index)}> 
+                {item.title} 
+                <i className='material-icons'>
+                  {
+                    services === index ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
+                  }
+                </i>
+              </span>
+              {
+                services === index && (<span> {item.desc} </span>)
+              }
+            </li>
+          ))}
         </ul>
       </div>
 
-      <div className="section-three">
+      <div className='section-three'>
         <h2>Our Products</h2>
 
         <ul>
-          {
-            [
-              "Elevators",
-              "Escalators",
-              "Supply gears",
-              'Bearings'
-            ].map(item => (
-            <li>{item}</li>
-            ))
-          }
+          {[
+            {
+              title: "Elevators",
+              desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+            },
+            {
+              title: "Escalators",
+              desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+            },
+            {
+              title: "Supply gears",
+              desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+            },
+            {
+              title: "Bearings",
+              desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+            },
+          ].map((item, index) => (
+            <li>
+              <span onClick={() => products === index ? toggleProducts(null) :  toggleProducts(index)}> 
+                {item.title} 
+                <i className='material-icons'>
+                  {
+                    services === index ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
+                  }
+                </i>
+              </span>
+              {
+                products === index && (<span> {item.desc} </span>)
+              }
+            </li>
+          ))}
         </ul>
       </div>
-
-
     </div>
-  )
+  );
 }
 
-export default ProductsServices
+export default ProductsServices;
